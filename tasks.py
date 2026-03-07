955308e9ab8699a1fb5c8f3f8e15605be018c31a
Daniel Edgecombe <edgy.edgemond@gmail.com>
2026-03-07 18:32:11 +0000

    chore: Add invoke tasks for installation, test and release management.

---
+++ tasks.py
@@ -0,0 +1,49 @@
+"""Collection of useful commands for code management.
+
+To view a list of available commands:
+
+$ invoke --list
+"""
+
+import invoke  # type: ignore[unresolved-import]
+
+
+@invoke.task
+def install(context):
+    """Install production requirements."""
+    context.run("uv sync")
+
+
+@invoke.task
+def install_dev(context):
+    """Install development requirements."""
+    context.run("uv sync --all-extras")
+    context.run("uv run prek install")
+    context.run("uv run prek install-hooks")
+
+
+@invoke.task
+def check_style(context):
+    """Run style checks."""
+    context.run("ruff check .")
+
+
+@invoke.task
+def tests(context):
+    """Run pytest unit tests."""
+    context.run("pytest -x -s")
+
+
+@invoke.task
+def tests_coverage(context):
+    """Run pytest unit tests with coverage."""
+    context.run("pytest --cov -x --cov-report=xml")
+
+
+@invoke.task
+def release(context):
+    """Generate changelog and tag for release."""
+    context.run("git-cliff --config pyproject.toml --bump -o CHANGELOG.md")
+    result = context.run("git-cliff --bumped-version")
+    x = result.stdout.splitlines()[0].lstrip("v")
+    context.run(f"bumpversion {x}")
